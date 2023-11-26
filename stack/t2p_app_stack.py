712847cf4f1e02e4047504070f68f84493d4b46b
alireza <alireza.es@gmail.com>
2023-11-25 22:09:09 -0700

    Fix dependency

--- stack/t2p_app_stack.py
+++ stack/t2p_app_stack.py
@@ -1,9 +1,8 @@
 import aws_cdk as cdk
 from constructs import Construct
-from aws_cdk.aws_lambda import Code, Runtime
+from aws_cdk.aws_lambda import Code, Runtime, Function
 from aws_cdk.aws_s3 import Bucket
 from constants import MODEL_FILE_NAME
-from aws_cdk import aws_lambda_python as _lambda_python
 
 class T2pAppStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, env_name: str, **kwargs) -> None:
@@ -12,10 +11,10 @@ class T2pAppStack(cdk.Stack):
 
         bucket = Bucket(self, "T2pBucket", bucket_name=f"{env_name}-t2p-ml-model")
 
-        _lambda_python.PythonFunction(self, "LambdaFunction",
+        Function(self, "LambdaFunction",
             function_name=f"{env_name}-t2p-ml-predict",
-            entry="src/functions",
-            index="predict.py",
+            handler="index.handler",
+            code=Code.from_asset("src/functions"),
             runtime=Runtime.PYTHON_3_8,
             environment={
                 "BUCKET_NAME": bucket.bucket_name,
@@ -24,14 +23,3 @@ class T2pAppStack(cdk.Stack):
         )
         
         from aws_cdk import aws_lambda_python as _lambda_python
-
-function = _lambda_python.PythonFunction(
-    self, "LambdaFunction",
-    entry="path/to/your/lambda/function/directory",
-    index="predict.py",
-    runtime=_lambda.Runtime.PYTHON_3_8,
-    environment={
-        "BUCKET_NAME": bucket.bucket_name,
-        "MODEL_FILE_NAME": "model.pkl"
-    }
-)
