d78b27bd4aa351e068cc3ec1a009db3df8734b76
alireza <alireza.es@gmail.com>
2023-11-25 22:34:07 -0700

    Update functions

--- stack/t2p_app_stack.py
+++ stack/t2p_app_stack.py
@@ -1,6 +1,10 @@
 import aws_cdk as cdk
 from constructs import Construct
-from aws_cdk.aws_lambda import Code, Runtime, Function
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+    aws_lambda_python_alpha as _lambda_python,
+)
 from aws_cdk.aws_s3 import Bucket
 from constants import MODEL_FILE_NAME
 
@@ -11,13 +15,15 @@ class T2pAppStack(cdk.Stack):
 
         bucket = Bucket(self, "T2pBucket", bucket_name=f"{env_name}-t2p-ml-model")
 
-        Function(self, "LambdaFunction",
+        _lambda_python.PythonFunction(self, "LambdaFunction",
             function_name=f"{env_name}-t2p-ml-predict",
-            handler="index.handler",
-            code=Code.from_asset("src/functions"),
-            runtime=Runtime.PYTHON_3_8,
+            entry="./src/functions",
+            index="predict.py",
+            handler="handler",
+            runtime=_lambda.Runtime.PYTHON_3_9,
             environment={
                 "BUCKET_NAME": bucket.bucket_name,
                 "MODEL_FILE_NAME": f"{env_name}-MODEL_FILE_NAME"
             }
         )
+        
